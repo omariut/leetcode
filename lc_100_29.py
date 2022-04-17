9e533bcb59466b43b2b6e4c147145cfb6d7edca1
omar <omarcpgcbl@gmail.com>
2022-04-17 13:01:08 +0600

    leetcode 29"

--- lc_100_29.py
+++ lc_100_29.py
@@ -2,8 +2,6 @@
 Runtime: 27 ms, faster than 96.00% of Python3 online submissions for Valid Parentheses.
 Memory Usage: 13.9 MB, less than 73.93% of Python3 online submissions for Valid Parentheses.
 """
-
-
 class Solution:
     def isValid(self, s: str) -> bool:
         if s[-1] == '(' or s[-1] == '{' or s[-1] == '[':
